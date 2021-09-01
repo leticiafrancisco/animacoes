025b167d91b5ffadf91b13f7055d2bea16dd6a7b
leticiafrancisco <leticia.santos1105@etec.sp.gov.br>
2021-08-31 22:43:38 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+ LayoutAnimation,
+ Platform,
+ StyleSheet,
+ Text,
+ TouchableOpacity,
+ UIManager,
+ View,
+ Image
+} from 'react-native';
 
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental){
+   UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+ const [expanded, setExpanded] = useState(false);
+ return (
+    <View style={style.container}>
+      <TouchableOpacity
+        onPress={() => {LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        setExpanded(!expanded)}
+        }
+      >
+        <Text>Toque em mim para {expanded ? 'retrair' : 'expandir'}!</Text>
+      </TouchableOpacity>
+      {expanded && (
+        <View style={style.tile}>
+          <Image style={styles.imagens} 
+            source={{ uri: 'https://cdn.pixabay.com/photo/2012/04/05/00/32/lemon-25342_960_720.png' }}
+          />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
+  tile: {
+    
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  imagens: {
+    width: 300,
+    height: 300,
+    alignItems: 'center',
+    marginBottom: 30
   },
 });
+    
+
+   
